5cf8972a09e75176c6476aee39527260ded4c0bf
Wannes Paesschesoone <wannespaesschesoone@gmail.com>
2025-10-27 00:38:23 +0100

    Re-ordered files and created a Dockerfile (will later be integraded in the compose) + started setting up influxdb in python script

--- app/collector.py
+++ app/collector.py
@@ -8,7 +8,12 @@ load_dotenv()
 HOMEWIZARD_IP = os.getenv('HOMEWIZARD_IP')
 HOMEWIZARD_TOKEN = os.getenv('HOMEWIZARD_TOKEN')
 
+INFLUXDB_TOKEN = os.getenv('INFLUXDB_ADMIN_TOKEN')
+INFLUXDB_ORG = os.getenv('INFLUXDB_ORG')
+INFLUXDB_BUCKET = os.getenv('INFLUXDB_BUCKET')
+
 def fetch_homewizard_data() -> Optional[dict[str, Any]]:
+    """Fetches latest measurements from homewizard device."""
     try:
         url = f"https://{HOMEWIZARD_IP}/api/measurement"
         headers = {
@@ -22,6 +27,12 @@ def fetch_homewizard_data() -> Optional[dict[str, Any]]:
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {e}")
         return None
+
+def write_to_influxdb(data: dict[str, Any]):
+    """Writes the fetched data into the InfluxDB bucket"""
+    if not data:
+        print("No data to write")
+        return
 
 if __name__ == "__main__":
     fetch_homewizard_data()
